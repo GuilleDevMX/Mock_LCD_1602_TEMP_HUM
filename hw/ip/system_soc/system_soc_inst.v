9e484ea89f9a0b7c69590e6930853224e54bc073
GuilleDevMX <1415063h@umich.mx>
2026-03-26 17:58:39 -0600

    Add system_soc and system_top modules for Nios V/c SoC integration
    - Created system_soc.v to define the SoC architecture, including CPU, memory, I2C, UART, and timer components.
    - Implemented system_top.v as a top-level wrapper for the SoC, integrating I2C tri-state logic and UART connections.
    - Established internal signals for I2C communication and ensured proper handling of open-drain outputs.

--- hw/ip/system_soc/system_soc_inst.v
+++ hw/ip/system_soc/system_soc_inst.v
@@ -1,11 +1,11 @@
 	system_soc u0 (
 		.clk_clk           (<connected-to-clk_clk>),           //          clk.clk
-		.reset_reset_n     (<connected-to-reset_reset_n>),     //        reset.reset_n
 		.com_export_sda_in (<connected-to-com_export_sda_in>), //   com_export.sda_in
 		.com_export_scl_in (<connected-to-com_export_scl_in>), //             .scl_in
 		.com_export_sda_oe (<connected-to-com_export_sda_oe>), //             .sda_oe
 		.com_export_scl_oe (<connected-to-com_export_scl_oe>), //             .scl_oe
 		.debug_export_rxd  (<connected-to-debug_export_rxd>),  // debug_export.rxd
-		.debug_export_txd  (<connected-to-debug_export_txd>)   //             .txd
+		.debug_export_txd  (<connected-to-debug_export_txd>),  //             .txd
+		.reset_reset_n     (<connected-to-reset_reset_n>)      //        reset.reset_n
 	);
 
